db1de9dfa3d7474168333a76896db1f5ab360710
Daniel Menet <membership@sontags.ch>
2019-12-24 08:45:53 +0100

    alterverse: added tests

--- alterverse_test.go
+++ alterverse_test.go
@@ -2,23 +2,50 @@ package main
 
 import (
 	"fmt"
+	"path/filepath"
 	"testing"
 )
 
+func TestNewAlterverse(t *testing.T) {
+	tests := []struct {
+		location    string
+		ignore      string
+		errExpected bool
+	}{
+		{location: "alterverse1", ignore: defaultIgnore, errExpected: false},
+		{location: "alterverse_does_not_exist", ignore: defaultIgnore, errExpected: true},
+		{location: "alterverse_manifest_missing", ignore: defaultIgnore, errExpected: true},
+		{location: "alterverse_is_file", ignore: defaultIgnore, errExpected: true},
+		{location: "alterverse_malformed_manifest", ignore: defaultIgnore, errExpected: true},
+	}
+
+	for _, test := range tests {
+		t.Run(test.location, func(t *testing.T) {
+			_, errs := NewAlterverse(filepath.Join(testdata, test.location), test.ignore)
+			hasErrs := len(errs) > 0
+			if hasErrs && !test.errExpected {
+				t.Errorf("has unexpected errors, errors are: %v", errs)
+			} else if !hasErrs && test.errExpected {
+				t.Errorf("errors expected but no errors orrured")
+			}
+		})
+	}
+}
+
 func TestValueDublicates(t *testing.T) {
 	t.Parallel()
-	tests := []struct {
+	tests := map[string]struct {
 		manifest          map[string]string
 		dublicateExpected bool
 	}{
-		{
+		"Basic": {
 			manifest: map[string]string{
 				"foo":  "bar",
 				"test": "bla",
 			},
 			dublicateExpected: false,
 		},
-		{
+		"WithSubstring": {
 			manifest: map[string]string{
 				"foo":    "bar",
 				"test":   "bla",
@@ -26,7 +53,7 @@ func TestValueDublicates(t *testing.T) {
 			},
 			dublicateExpected: false,
 		},
-		{
+		"WithDublicate": {
 			manifest: map[string]string{
 				"foo":    "bar",
 				"test":   "bla",
@@ -37,8 +64,8 @@ func TestValueDublicates(t *testing.T) {
 		},
 	}
 
-	for nr, test := range tests {
-		t.Run(fmt.Sprintf("%d", nr), func(t *testing.T) {
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
 			a := Alterverse{Manifest: test.manifest}
 			errs := a.HasValueDublicates()
 			hasDublicates := false
