12c2da2055949caadd06682a3d3c609d5d83ebad
Daniel Menet <membership@sontags.ch>
2020-01-04 22:36:50 +0100

    version: moved to own file

--- main.go
+++ main.go
@@ -5,15 +5,6 @@ import (
 	"os"
 )
 
-var (
-	// These variables are passed during `go build` via ldflags, for example:
-	//   go build -ldflags "-X main.commit=$(git rev-list -1 HEAD)"
-	// goreleaser (https://goreleaser.com/) does this by default.
-	version string
-	commit  string
-	date    string
-)
-
 func main() {
 	err := NewApp().Execute()
 	exitOnErr(err)
@@ -35,19 +26,4 @@ func exitOnErr(errs ...error) {
 		fmt.Print("\n")
 		os.Exit(-1)
 	}
-}
-
-// verisonInfo returns a string containing information usually passed via
-// ldflags during build time.
-func versionInfo() string {
-	if version == "" {
-		version = "dirty"
-	}
-	if commit == "" {
-		commit = "dirty"
-	}
-	if date == "" {
-		date = "unknown"
-	}
-	return fmt.Sprintf("Version:    %s\nCommit:     %s\nBuild Date: %s\n", version, commit, date)
 }
